15ea033150959b57de02871c80ef98fe702cb70f
Mustapha <elmustapha.abourar@outlook.com>
2024-05-02 11:15:04 +0200

    Setup Authentication using clerk

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,20 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
-
-export default clerkMiddleware({
+import { authMiddleware } from "@clerk/nextjs/server";
+export default authMiddleware({
+    // "/" will be accessible to all users
     publicRoutes: [
-        '/',
-        '/events/:id',
+        "/",
+        "events/:id",
+        "api/webhooks/clerk",
+        "/api/webhook/stripe",
+        '/api/uploadthing',
+    ],
+    ignoredRoutes: [
         '/api/webhooks/clerk',
-    ]
+        '/api/webhook/stripe',
+        '/api/uploadthing',
+    ],
 });
 
 export const config = {
-    matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+    matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
