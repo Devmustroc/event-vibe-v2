b83e32a9fdf8de79ec8c59c911ded71db07e9307
Mustapha <elmustapha.abourar@outlook.com>
2024-05-02 10:29:34 +0200

    Setup Environment

---
+++ app/(root)/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import {Button} from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main>
+      <h1
+        className="text-4xl"
+      >
+        EventVibe
+      </h1>
+      <Button>Hello</Button>
+    </main>
+  );
+}
